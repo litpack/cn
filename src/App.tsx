84c1f91fa31a0fbbada216676e10902ad81e36d4
mehrabix <mehrabi@post.com>
2024-11-21 23:18:11 +0330

    chore: update styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import "./components/ui/button";
 
 const App: Component = () => {
 
-  
+
   return (
     <>
-    <cn-button variant="outline" size="lg" label="Submit"></cn-button>
-    <cn-button variant="ghost" disabled="true" label="Cancel"></cn-button>
+      <cn-button variant="default" size="default" label="Default"></cn-button>
     </>
   );
 };
