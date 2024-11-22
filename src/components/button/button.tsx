fdb5260fcb8746f4fdf5a9b432c0c5eb8ac052d5
mehrabix <mehrabi@post.com>
2024-11-22 11:43:42 +0330

    improve button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,19 +2,17 @@ import { customElement } from "solid-element";
 import { mergeProps } from "solid-js";
 import globalCSS from "../../index.css?inline";
 import { cva, type VariantProps } from "class-variance-authority";
+import { JSXElement } from 'solid-js';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
@@ -36,6 +34,8 @@ interface ButtonProps extends VariantProps<typeof buttonVariants> {
   disabled?: boolean;
   onClick?: (e: MouseEvent) => void;
   label?: string;
+  asChild?: boolean;
+  children?: JSXElement;
 }
 
 function Button(props: ButtonProps) {
@@ -45,6 +45,7 @@ function Button(props: ButtonProps) {
       size: "default" as VariantProps<typeof buttonVariants>["size"],
       disabled: false,
       label: "",
+      asChild: false,
     },
     props
   );
@@ -52,16 +53,19 @@ function Button(props: ButtonProps) {
   return (
     <>
       <style>{globalCSS}</style>
-      <button
-        class={buttonVariants({
-          variant: mergedProps.variant,
-          size: mergedProps.size,
-        })}
-        disabled={mergedProps.disabled}
-        onClick={mergedProps.onClick}
-      >
-        {mergedProps.label}
-      </button>
+      {mergedProps.asChild ? (
+        <div class={buttonVariants({ variant: mergedProps.variant, size: mergedProps.size })}>
+          {props.children}
+        </div>
+      ) : (
+        <button
+          class={buttonVariants({ variant: mergedProps.variant, size: mergedProps.size })}
+          disabled={mergedProps.disabled}
+          onClick={mergedProps.onClick}
+        >
+          {mergedProps.label}
+        </button>
+      )}
     </>
   );
 }
@@ -74,7 +78,9 @@ customElement(
     disabled: undefined,
     label: undefined,
     onClick: undefined,
+    asChild: false,
   },
-  Button,
+  Button
 );
-export { Button };
+
+export { Button, buttonVariants };
